ac4e9291eb6ae51c326a5ef7b469c513485bcf3e
qhy <271467387@qq.com>
2016-04-26 16:13:51 +0800

    关于MatchActivity的更新

--- ProjectN/app/src/main/java/com/sora/projectn/model/Activity/MatchActivity.java
+++ ProjectN/app/src/main/java/com/sora/projectn/model/Activity/MatchActivity.java
@@ -2,9 +2,25 @@ package com.sora.projectn.model.Activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+import android.widget.Toolbar;
 
 import com.sora.projectn.R;
+import com.sora.projectn.utils.GetHttpResponse;
+import com.sora.projectn.utils.MatchPlayerAdapter;
+import com.sora.projectn.utils.MatchTeamAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Sora on 2016/2/6.
@@ -12,12 +28,27 @@ import com.sora.projectn.R;
 public class MatchActivity extends AppCompatActivity{
 
     private int no;
+    private int matchId = 2015102701;
+    private int hometeamId = 0 , customteamId = 0;
+    private static final int GET_DATA = 0x01;
+    private static final String serverIP = "192.168.31.225";
+    private static final String serverPORT = "8080";
+    private static final String databaseNAME = "NBADataSystem";
+    private static final String getmatchInfo = "getTeamMatchStatistics.do";
+    private static final String getplayerInfo = "getPlayerMatchStatistics.do";
+    private List<Map<String,String>> match_team_info = new ArrayList<Map<String,String>>();
+    private List<Map<String,String>> match_hometeam_playerinfos = new ArrayList<Map<String,String>>();
+    private List<Map<String,String>> match_customteam_playerinfos = new ArrayList<Map<String,String>>();
+
+    private Toolbar toolbar;
+
+    private ListView match_teaminfo_lv,match_hometeam_playerinfo_lv,match_customteam_playerinfo_lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_match);
-
+        initview();
         parseIntent();
 
         new Thread(new Runnable() {
@@ -34,6 +65,174 @@ public class MatchActivity extends AppCompatActivity{
      * 获取数据
      */
     private void getData() {
+        getmatchteaminfo();
+        getmatchplayerinfo();
+        handler.sendEmptyMessage(GET_DATA);
+    }
+
+
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what){
+                case GET_DATA:
+                    //TODO 让界面显示比赛信息
+                    MatchTeamAdapter mtadapter = new MatchTeamAdapter(match_team_info,MatchActivity.this);
+                    match_teaminfo_lv.setAdapter(mtadapter);
+                    MatchPlayerAdapter mpadapter1 = new MatchPlayerAdapter(match_hometeam_playerinfos,MatchActivity.this);
+                    match_hometeam_playerinfo_lv.setAdapter(mpadapter1);
+                    MatchPlayerAdapter mpadapter2 = new MatchPlayerAdapter(match_customteam_playerinfos,MatchActivity.this);
+                    match_customteam_playerinfo_lv.setAdapter(mpadapter2);
+            }
+        }
+    };
+
+    private void getmatchteaminfo(){
+        List<Map<String, String>> match_teaminfo = new ArrayList<Map<String,String>>();
+        String url = "http://"+serverIP+":"+serverPORT+"/"+databaseNAME+"/"+getmatchInfo+"?"+"matchId="+matchId;
+        String jsonstring = GetHttpResponse.getHttpResponse(url);
+        try {
+            JSONArray array = new JSONArray(jsonstring);
+            for (int i = 0; i < array.length(); i++) {
+                Map<String,String> temp = new HashMap<String,String>();
+                JSONObject obj = array.getJSONObject(i);
+                String matchId = obj.getString("matchId");
+                String teamId = obj.getString("teamId");
+                String ifHome = obj.getString("ifHome");
+                String time = obj.getString("time");
+                String twoHit = obj.getString("twoHit");
+                String twoShot = obj.getString("twoShot");
+                String threeHit = obj.getString("threeHit");
+                String threeShot = obj.getString("threeShot");
+                String freeThrowHit = obj.getString("freeThrowHit");
+                String freeThrowShot = obj.getString("freeThrowShot");
+                String offReb = obj.getString("offReb");
+                String defReb = obj.getString("defReb");
+                String ass = obj.getString("ass");
+                String steal = obj.getString("steal");
+                String blockShot = obj.getString("blockShot");
+                String turnOver = obj.getString("turnOver");
+                String foul = obj.getString("foul");
+                String score = obj.getString("score");
+
+                temp.put("matchId",matchId);
+                temp.put("teamId",teamId);
+                temp.put("ifHome",ifHome);
+                if(customteamId == 0 && Integer.parseInt(ifHome) == 0){
+                    customteamId = Integer.parseInt(teamId);
+                }
+                else if(hometeamId == 0 && Integer.parseInt(ifHome) == 1){
+                    hometeamId = Integer.parseInt(teamId);
+                }
+                temp.put("time",time);
+                temp.put("twoHit",twoHit);
+                temp.put("twoShot",twoShot);
+                temp.put("threeHit",threeHit);
+                temp.put("threeShot",threeShot);
+                temp.put("freeThrowHit",freeThrowHit);
+                temp.put("freeThrowShot",freeThrowShot);
+                temp.put("offReb",offReb);
+                temp.put("defReb",defReb);
+                temp.put("ass",ass);
+                temp.put("steal",steal);
+                temp.put("blockShot",blockShot);
+                temp.put("turnOver",turnOver);
+                temp.put("foul",foul);
+                temp.put("score",score);
+
+
+
+                match_teaminfo.add(temp);
+            }
+
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+            match_teaminfo = null;
+        }
+        this.match_team_info = match_teaminfo;
+    }
+
+    private void getmatchplayerinfo(){
+
+        List<Map<String, String>> match_playerinfo = new ArrayList<Map<String,String>>();
+        String url = "http://"+serverIP+":"+serverPORT+"/"+databaseNAME+"/"+getplayerInfo+"?"+"matchId="+matchId;
+        String jsonstring = GetHttpResponse.getHttpResponse(url);
+        try {
+            JSONArray array = new JSONArray(jsonstring);
+            for (int i = 0; i < array.length(); i++) {
+                Map<String,String> temp = new HashMap<String,String>();
+                JSONObject obj = array.getJSONObject(i);
+                String matchId = obj.getString("matchId");
+                String playerId = obj.getString("playerId");
+                String teamId = obj.getString("teamId");
+                String playerName = obj.getString("playerName");
+                String isFirst = obj.getString("isFirst");
+                String time = obj.getString("time");
+                String twoHit = obj.getString("twoHit");
+                String twoShot = obj.getString("twoShot");
+                String threeHit = obj.getString("threeHit");
+                String threeShot = obj.getString("threeShot");
+                String freeThrowHit = obj.getString("freeThrowHit");
+                String freeThrowShot = obj.getString("freeThrowShot");
+                String offReb = obj.getString("offReb");
+                String defReb = obj.getString("defReb");
+                String ass = obj.getString("ass");
+                String steal = obj.getString("steal");
+                String blockShot = obj.getString("blockShot");
+                String turnOver = obj.getString("turnOver");
+                String foul = obj.getString("foul");
+                String score = obj.getString("score");
+
+                temp.put("playerName",playerName);
+                temp.put("playerId",playerId);
+                temp.put("matchId",matchId);
+                temp.put("teamId",teamId);
+                temp.put("isFirst",isFirst);
+                temp.put("time",time);
+                temp.put("twoHit",twoHit);
+                temp.put("twoShot",twoShot);
+                temp.put("threeHit",threeHit);
+                temp.put("threeShot",threeShot);
+                temp.put("freeThrowHit",freeThrowHit);
+                temp.put("freeThrowShot",freeThrowShot);
+                temp.put("offReb",offReb);
+                temp.put("defReb",defReb);
+                temp.put("ass",ass);
+                temp.put("steal",steal);
+                temp.put("blockShot",blockShot);
+                temp.put("turnOver",turnOver);
+                temp.put("foul",foul);
+                temp.put("score",score);
+
+
+
+                 match_playerinfo.add(temp);
+            }
+
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+            match_playerinfo = null;
+        }
+        for(Map<String,String> t:match_playerinfo){
+            if(Integer.parseInt(t.get("teamId")) == hometeamId){
+                match_hometeam_playerinfos.add(t);
+            }
+            else if(Integer.parseInt(t.get("teamId")) == customteamId){
+                match_customteam_playerinfos.add(t);
+            }
+        }
+
+    }
+
+    private void initview(){
+        match_teaminfo_lv = (ListView)findViewById(R.id.match_pts);
+        match_hometeam_playerinfo_lv = (ListView)findViewById(R.id.match_2);
+        match_customteam_playerinfo_lv = (ListView)findViewById(R.id.match_3);
 
     }
 
@@ -45,5 +244,6 @@ public class MatchActivity extends AppCompatActivity{
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
         no = bundle.getInt("no");
+        matchId = no;
     }
 }
---
+++ ProjectN/app/src/main/java/com/sora/projectn/utils/MatchPlayerAdapter.java
@@ -0,0 +1,102 @@
+package com.sora.projectn.utils;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.sora.projectn.R;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by qhy on 2016/4/26.
+ */
+public class MatchPlayerAdapter extends BaseAdapter {
+
+    private List<Map<String,String>> match_player_infos;
+    private LayoutInflater inflater;
+    public MatchPlayerAdapter(List<Map<String,String>> match_player_infos ,Context context){
+        this.match_player_infos = match_player_infos;
+        inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public int getCount() {
+        return match_player_infos.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return match_player_infos.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        // TODO Auto-generated method stub
+        // listview每次得到一个item，都要view去绘制，通过getView方法得到view
+        // position为item的序号
+        ViewHolder vh = null;
+        View view = null;
+        if (convertView != null) {
+            view = convertView;
+            vh = (ViewHolder) convertView.getTag();
+            // 使用缓存的view,节约内存
+            // 当listview的item过多时，拖动会遮住一部分item，被遮住的item的view就是convertView保存着。
+            // 当滚动条回到之前被遮住的item时，直接使用convertView，而不必再去new view()
+
+        } else {
+//            view = super.getView(position, convertView, parent);
+            view = inflater.inflate(R.layout.match_playerinfo_item, null);
+            vh = new ViewHolder(view);
+            view.setTag(vh);
+        }
+        vh.player_two.setText(String.valueOf(Integer.parseInt(match_player_infos.get(position).get("twoHit"))*2));
+        vh.player_three.setText(String.valueOf(Integer.parseInt(match_player_infos.get(position).get("threeHit"))*2));
+        vh.player_name.setText(match_player_infos.get(position).get("playerName"));
+        vh.player_foul.setText(match_player_infos.get(position).get("foul"));
+        vh.player_blockshot.setText(match_player_infos.get(position).get("blockShot"));
+        vh.player_ass.setText(match_player_infos.get(position).get("ass"));
+        vh.player_turnover.setText(match_player_infos.get(position).get("turnOver"));
+        vh.player_reb.setText(match_player_infos.get(position).get("reb"));
+        vh.player_score.setText(match_player_infos.get(position).get("score"));
+        vh.player_steal.setText(match_player_infos.get(position).get("steal"));
+
+
+        int[] colors = { Color.WHITE, Color.rgb(219, 238, 244) };//RGB颜色
+
+        view.setBackgroundColor(colors[position % 2]);// 每隔item之间颜色不同
+
+        return view;
+    }
+
+    private class ViewHolder {
+
+        TextView player_name,player_score,player_two,player_three,player_reb,player_ass,player_steal,player_blockshot,player_turnover,player_foul;
+
+        ViewHolder(View view){
+            player_ass = (TextView)view.findViewById(R.id.match_player_ass);
+            player_blockshot = (TextView)view.findViewById(R.id.match_player_blockshot);
+            player_foul =(TextView) view.findViewById(R.id.match_player_foul);
+            player_name = (TextView)view.findViewById(R.id.match_player_name);
+            player_reb = (TextView)view.findViewById(R.id.match_player_reb);
+            player_score = (TextView)view.findViewById(R.id.match_player_score);
+            player_steal =(TextView) view.findViewById(R.id.match_player_steal);
+            player_three =(TextView) view.findViewById(R.id.match_player_three);
+            player_turnover = (TextView)view.findViewById(R.id.match_player_turnover);
+            player_two = (TextView)view.findViewById(R.id.match_player_two);
+
+
+        }
+    }
+
+}
